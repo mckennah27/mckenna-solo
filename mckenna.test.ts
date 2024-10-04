69e8015b6b4980719455ea9bb0c222a1f4c12995
McKenna H <mckennah@McKennas-Laptop.local>
2024-10-03 23:01:26 -0600

    Update

--- mckenna.test.ts
+++ mckenna.test.ts
@@ -7,10 +7,55 @@ test('first test', async () => {
     await mp.navigate();
     await mp.click(mp.newsletter);
     await mp.click(mp.name);
+    await mp.myName('McKenna Hawks');
     await mp.click(mp.email);
-    await mp.click(mp.country);
+    await mp.myEmail('mchaw27@gmail.com');
     await mp.click(mp.checkBox);
-    await mp.click(mp.submit);
+    await fs.writeFile(`${__dirname}/stephenSignUp.png`,
+        await mp.driver.takeScreenshot(), 'base64', (e) => {
+            if (e) console.error(e)
+            else console.log('page saved');
+        });
+    await mp.driver.sleep(3000)
+    //await mp.driver.quit();
     
+});
+
+test('second test', async () => {
+    await mp.navigate();
+    await mp.click(mp.works);
+    await mp.click(mp.filmTv);
+    await mp.click(mp.other);
+    await mp.click(mp.writtenWorks);
+    //await mp.driver.quit();
+});
+
+test('third test', async () => {
+    await mp.navigate();
+    await mp.click(mp.upcoming);
+    await mp.click(mp.movies);
+    await mp.click(mp.novellas);
+    //await mp.driver.quit();
     
+});
+
+test('fourth test', async () => {
+   await mp.navigate();
+   await mp.click(mp.theAuthor);
+   await mp.click(mp.pressBiography);
+   await mp.click(mp.photo);
+   await mp.click(mp.filmTvAppearances);
+   await mp.click(mp.awards);
+   await mp.click(mp.about);
+   //await mp.driver.quit();
+
+});
+
+test('fifth test', async () =>{
+    await mp.navigate();
+    await mp.click(mp.miscellaneous);
+    await mp.click(mp.links);
+    await mp.click(mp.credits);
+    await mp.driver.quit();
+
 })
--- mckennaPage.ts
+++ mckennaPage.ts
@@ -10,6 +10,30 @@ export class McKennaPage extends BasePage {
     country: By = By.xpath('//select[@id="dropdownCountry"]')
     checkBox:By = By.xpath('(//input[@id="check"])[1]')
     submit:By = By.xpath('//button[@class="btn"]')
+    //second test
+    works:By = By.xpath('(//a[text()="Works"])[1]')
+    filmTv:By = By.xpath('(//span[text()="Film & TV"])[1]')
+    other:By = By.xpath('(//span[text()="Other"])[1]')
+    writtenWorks: By = By.xpath('(//span[text()="Written Works"])[1]')
+    //third test
+    upcoming:By = By.xpath('(//a[text()="Upcoming"])[1]')
+    movies:By = By.xpath('(//a[@class = "nav-link"])[2]') 
+    novellas:By = By.xpath('(//a[@class = "scroller active"][1])')
+    //fourth test
+    theAuthor:By = By.xpath('(//a[text()="The Author"])[1]')
+    pressBiography:By = By.xpath('(//a[text()="Press Biography"])[1]')
+    photo:By = By.xpath('(//a[text()="Photo Gallery"])[1]')
+    filmTvAppearances:By = By.xpath('(//a[text()="Film/TV Appearances"])[1]')
+    awards:By = By.xpath('(//a[text()="Awards"])[1]')
+    about:By = By.xpath('(//a[text()="About the Author"])[1]')
+    //fifth test
+    miscellaneous:By = By.xpath('(//a[text()="Miscellaneous"])[1]')
+    links:By = By.xpath('(//a[text()="Links"])[1]')
+    credits:By = By.xpath('(//a[text()="Credits"])[1]')
+
+
+
+
 
 
 
@@ -17,13 +41,18 @@ export class McKennaPage extends BasePage {
     constructor() {
         super({url:"https://stephenking.com/"});
     };
+    
+    //methods
+    async myName(text: string) {
+        return this.setInput(this.name, `${text}`);
+    };
+    
+    async myEmail(textEmail: string) {
+        return this.setInput(this.email, `${textEmail}`);
 
+    }
 
 };
 
 
-    //methods
-
-
-
-}
+   
